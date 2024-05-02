ab8510d80c1d2652ada983e28fb14b5c2d2f10da
Kimjuyoung <jjassb404@naver.com>
2024-05-02 22:38:28 +0900

    303. Range Sum Query - Immutable

--- LEETCODE/303. Range Sum Query - Immutable.js
+++ LEETCODE/303. Range Sum Query - Immutable.js
@@ -5,6 +5,7 @@ class NumArray {
         this.data = numbers;
     }
 
+    
     sumRange(l, r) { //l,r 인덱스 사이 값 합 계산. for 함수 사용해서 루프
         let sum = 0;
         for (let i = l; i <= r; i++) sum += this.data[i];
