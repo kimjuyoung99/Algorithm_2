3a4424b26d5f9e97ac93f30ba6da9a6790577cec
Kimjuyoung <jjassb404@naver.com>
2024-05-07 14:00:09 +0900

    구명보트 수정

--- PROGREMMERS/LEVEL_2/구명보트.js
+++ PROGREMMERS/LEVEL_2/구명보트.js
@@ -1,22 +1,29 @@
+
 function solution(people, limit) {
-    console.log("지금이니?");
+    people.sort((a, b) => a - b);//오름차순 정렬
     var answer = 0;
     for(let i = 0 ; i < people.length ; i++){
-        for(let j = 0 ; j < people.length ; j++){
-            if(people[i]+people[j] <= limit){
+        for(let j = i ; j < people.length ; j++){
+            if(i!=j && people[i]+people[j] <= limit && people[i]!=-1 && people[j]!=-1){
                 answer++;
                 people[i]=-1;
                 people[j]=-1;
+                console.log("answer : "+ answer)
+
             }
         }
     }
     for(let i = 0 ; i < people.length ; i++){
         if(people[i] != -1) answer++;
+        console.log("answer : "+ answer)
     }
     console.log("answer : "+ answer)
     
     return answer;
 }
+let people = [70, 80, 50];
+let limit = 100;
+solution(people, limit);
 
 //1. 우선, 2중 for문을 쭉 돌면서 2명이 한 개 구명보트 탈 수 있는 경우를 찾는다.
 //2.1번의 경우를 찾을 경우 answer +1 , 그리고 그 두 명에 해당하는 people 배열 자리는 '-1'처리 한다. 
