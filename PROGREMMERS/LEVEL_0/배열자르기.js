a757991e6e99d44330e622ece67b01d82902156c
kimjuyoung99 <jjassb404@naver.com>
2024-09-06 11:10:28 +0900

    배열자르기

---
+++ PROGREMMERS/LEVEL_0/배열자르기.js
@@ -0,0 +1,15 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/120833
+function solution(numbers, num1, num2) {
+    return numbers.splice(num1,num2-num1+1);
+}
+
+
+// function solution(numbers, num1, num2) {
+//     var answer = [];
+    
+//     for(let i = num1 ; i <= num2 ; i++ ){
+//         answer.push(numbers[i]);
+//     }
+    
+//     return answer;
+// }
