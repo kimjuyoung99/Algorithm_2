ed265ef1c3497cb1f1185c3d37332834ee2653b6
kimjuyoung99 <jjassb404@naver.com>
2024-09-16 03:46:23 +0900

    모음제거

---
+++ PROGREMMERS/LEVEL_0/모음제거.js
@@ -0,0 +1,6 @@
+function solution(my_string) {
+    return my_string.replace(/[aeiou]/g,'');
+}
+//정규표현식 짜증난다잉 slice 밖에 다른 방법이 없는건가
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120849
