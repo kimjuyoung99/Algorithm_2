37fb48a927fc5ac9894780e8194858e96c2d64d6
kimjuyoung99 <jjassb404@naver.com>
2024-11-18 19:52:08 +0900

    A로 B만들기

---
+++ PROGREMMERS/LEVEL_0/A로 B 만들기.js
@@ -0,0 +1,5 @@
+function solution(before, after) {
+    if(before.split('').sort().join('')==after.split('').sort().join('')) return 
+    return 0;
+}
+// https://school.programmers.co.kr/learn/courses/30/lessons/120886?language=javascript
